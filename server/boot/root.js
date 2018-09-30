4171b01fb3d972647d4f858a724239873c85f821
GorilaNaranja <woodworkerdeveloper@gmail.com>
2018-09-30 15:17:44 +0200

    Root

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,40 +1,8 @@
 'use strict';
 
-module.exports = function(server) {
-  // Install a `/` route that returns server status
-  var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
-  server.use(router);
-};
-
-// app.post('/login', function(req, res) {
-//   User.login({
-//     email: req.body.email,
-//     password: req.body.password
-//   }, 'user', function(err, token) {
-//
-//     if (err) {
-//       res.render('response', { //render view named 'response.ejs'
-//         title: 'Login failed',
-//         content: err,
-//         redirectTo: '/',
-//         redirectToLinkText: 'Try again'
-//       });
-//       return;
-//     }
-//
-//     res.render('home', { //login user and render 'home' view
-//       email: req.body.email,
-//       accessToken: token.id
-//     });
-//   });
-// });
-//
-//
-// app.get('/logout', function(req, res, next) {
-//   if (!req.accessToken) return res.sendStatus(401); //return 401:unauthorized if accessToken is not present
-//   User.logout(req.accessToken.id, function(err) {
-//     if (err) return next(err);
-//     res.redirect('/'); //on successful logout, redirect
-//   });
-// });
+module.exports = function(app) {
+  // Install a "/ping" route that returns "pong"
+  app.get('/ping', function(req, res) {
+    res.send('pong');
+  });
+}
